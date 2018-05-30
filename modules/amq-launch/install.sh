20ff0551d86ceebf253b4a4d89fb01b2d655656e
Vanessa Busch <vbusch@redhat.com>
2018-05-30 18:37:43 -0400

    reindent shell script files
    Signed-off-by: Vanessa Busch <vbusch@redhat.com>

--- modules/amq-launch/install.sh
+++ modules/amq-launch/install.sh
@@ -14,11 +14,11 @@ mkdir -p ${DEST}
 mkdir -p ${DEST}/conf/
 
 cp -p ${SOURCES_DIR}/openshift-ping-common-$VERSION.jar \
-      ${SOURCES_DIR}/openshift-ping-dns-$VERSION.jar \
-    ${DEST}/lib
-	
+  ${SOURCES_DIR}/openshift-ping-dns-$VERSION.jar \
+  ${DEST}/lib
+
 cp -p $ADDED_DIR/jgroups-ping.xml \
-	${DEST}/conf/	
+  ${DEST}/conf/ 
 
 cp $ADDED_DIR/launch.sh ${ADDED_DIR}/readinessProbe.sh ${ADDED_DIR}/drain.sh $AMQ_HOME/bin
 chmod 0755 $AMQ_HOME/bin/launch.sh
