b73b5d93c075d4afaf7118343f8ebf04836c58d2
Roddie Kieley <rkieley@apache.org>
2019-01-03 15:39:44 -0330

    ENTMQBR-2159: Reversed the responsibility to pass an argument to launch.sh from s2i to drain.sh

--- modules/amq-broker-s2i/added/configure_s2i_files.sh
+++ modules/amq-broker-s2i/added/configure_s2i_files.sh
@@ -4,5 +4,5 @@ set -e
 INSTANCE_DIR=$1
 echo "Copying Config files from S2I build"
 cp -v $AMQ_HOME/conf/* ${INSTANCE_DIR}/etc/
-echo "Configuring S2I run to start"
-sed -i 's/launch\.sh/launch\.sh start/' /usr/local/s2i/run
+#echo "Configuring S2I run to start"
+#sed -i 's/launch\.sh/launch\.sh start/' /usr/local/s2i/run
