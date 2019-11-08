40b18389d02de63252ecfe391d7dd42d5c88905d
Roddie Kieley <rkieley@apache.org>
2019-11-08 09:09:08 -0330

    ENTMQBR-3039: Removing erronous removal of -XX:UseG1GC for s390x.

--- modules/amq-launch/added/launch.sh
+++ modules/amq-launch/added/launch.sh
@@ -20,9 +20,6 @@ JAVA_OPTS=$(echo $JAVA_OPTS | sed -e "s/-XX:+UseParallelOldGC/ /")
 PLATFORM=`uname -m`
 echo "Platform is ${PLATFORM}"
 if [ "${PLATFORM}" = "s390x" ] ; then
-  #GC Option found to be a problem on s390x
-  echo "Removing -XX:+UseG1GC as per ENTMQBR-3048"
-  JAVA_OPTS=$(echo $JAVA_OPTS | sed -e "s/-XX:+UseG1GC/ /")
   #JDK11 related warnings removal
   echo "Adding -Dcom.sun.xml.bind.v2.bytecode.ClassTailor.noOptimize=true as per ENTMQBR-1932"
   JAVA_OPTS="-Dcom.sun.xml.bind.v2.bytecode.ClassTailor.noOptimize=true ${JAVA_OPTS}"
