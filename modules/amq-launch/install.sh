31a121ba16e7525fef063b73f4315744eb9c6b56
Rafiur Rashid <rrashid@redhat.com>
2021-06-09 07:53:55 +0000

    Adding netty-tcnative-ppcle_64 source directory name

--- modules/amq-launch/install.sh
+++ modules/amq-launch/install.sh
@@ -17,6 +17,7 @@ cp -p ${SOURCES_DIR}/openshift-ping-common-$VERSION.jar \
   ${SOURCES_DIR}/openshift-ping-dns-$VERSION.jar \
   ${SOURCES_DIR}/netty-tcnative-2.0.29.Final-redhat-00001-linux-x86_64-fedora.jar \
   ${SOURCES_DIR}/netty-tcnative-2.0.34.Final-redhat-00001-linux-s390_64-fedora.jar \
+  ${SOURCES_DIR}/netty-tcnative-2.0.34.Final-redhat-00001-linux-ppcle_64-fedora.jar \
   ${DEST}/lib
 
 cp -p $ADDED_DIR/jgroups-ping.xml \
