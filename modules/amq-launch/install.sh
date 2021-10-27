ccc16393ef084dc58c8b4fbdbd4aa1b34f0e2d21
Howard Gao <howard.gao@gmail.com>
2021-10-27 15:26:30 +0800

    ENTMQBR-5639 Update for 7.9.1 aligned release

--- modules/amq-launch/install.sh
+++ modules/amq-launch/install.sh
@@ -15,7 +15,7 @@ mkdir -p ${DEST}/conf/
 
 cp -p ${SOURCES_DIR}/openshift-ping-common-$VERSION.jar \
   ${SOURCES_DIR}/openshift-ping-dns-$VERSION.jar \
-  ${SOURCES_DIR}/netty-tcnative-2.0.40.Final-redhat-00001-linux-x86_64-fedora.jar \
+  ${SOURCES_DIR}/netty-tcnative-2.0.42.Final-redhat-00001-linux-x86_64-fedora.jar \
   ${DEST}/lib
 
 cp -p $ADDED_DIR/jgroups-ping.xml \
