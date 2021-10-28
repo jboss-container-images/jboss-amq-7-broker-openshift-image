635b198cd7ad821111dd7a01debd33cf05fcc8df
Roddie Kieley <rkieley@apache.org>
2021-10-28 10:38:43 -0230

    Post-PR79 x86_64 readdition.

--- modules/amq-launch/install.sh
+++ modules/amq-launch/install.sh
@@ -15,6 +15,7 @@ mkdir -p ${DEST}/conf/
 
 cp -p ${SOURCES_DIR}/openshift-ping-common-$VERSION.jar \
   ${SOURCES_DIR}/openshift-ping-dns-$VERSION.jar \
+  ${SOURCES_DIR}/netty-tcnative-2.0.40.Final-redhat-00001-linux-x86_64-fedora.jar \
   ${SOURCES_DIR}/netty-tcnative-2.0.40.Final-redhat-00001-linux-s390_64-fedora.jar \
   ${SOURCES_DIR}/netty-tcnative-2.0.40.Final-redhat-00001-linux-ppcle_64-fedora.jar \
   ${DEST}/lib
